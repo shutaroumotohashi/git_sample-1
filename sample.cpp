16c4ae4d49a019b0170e156e046f31d85b13defc
Shutarou_Motohashi <motohashi_syu512@fuji.waseda.jp>
2020-06-15 20:34:52 +0900

    add multiplemetod

--- sample.cpp
+++ sample.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 
 int sum(int, int);
+int multiple(int, int);
 
 int main()
 {
     int a=5; int b=3;
     std::cout << sum(a,b) << std::endl;
 
-    a=2;
-    b=6;
-    std::cout << sum(a,b) << std::endl;
+    std::cout << multiple(a,b) << std::endl;
 
     return 0;
 }
@@ -17,4 +16,9 @@ int main()
 int sum(int a, int b)
 {
     return a+b;
+}
+
+int multiple(int a, int b)
+{
+    return a*b;
 }
